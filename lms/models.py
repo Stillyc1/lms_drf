488e324fbfe8eae49485df25bb47173f7ba6981c
Alexander_Baranov <Sti_ll@mail.ru>
2024-12-12 18:54:57 +0300

    add models lms, users

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(verbose_name='Название')
+    picture = models.ImageField(verbose_name='Превью')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    title = models.CharField(verbose_name='Название')
+    picture = models.ImageField(verbose_name='Превью')
+    description = models.TextField(verbose_name='Описание')
+    video = models.URLField(verbose_name='ссылка на видео')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="courses", verbose_name="курс")
+
+    def __str__(self):
+        return f"{self.title}"
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
--- users/models.py
+++ users/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
